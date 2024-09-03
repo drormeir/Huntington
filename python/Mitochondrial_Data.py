c36a6d86fcfcdc9f294481e85753b3a0fa6e2869
Dror Meirovich <dror@drorm.com>
2024-09-03 20:22:47 +0300

    Fix verbose level management

--- python/Mitochondrial_Data.py
+++ python/Mitochondrial_Data.py
@@ -6,13 +6,13 @@ from python.patients_wells import Patient_Wells_Collecter
 from python.Patients_Catalog import Patients_Catalog
 
 class Protein_Data:
-    def __init__(self, file_name, verbose: bool = False) -> None:
+    def __init__(self, file_name, verbose: int = 0) -> None:
         self.name, _ = os.path.splitext(os.path.basename(file_name))
         if verbose:
             print(f'Reading Mitochondrial protein data: {self.name}')
-        csv_data = csv_raw_data(file_name, verbose=False)
-        self.df = csv_header_body_2_dataframe(csv_data[0], csv_data[1:], verbose=verbose)
-        self.df['Patient_ID'] = Patients_Catalog(verbose=False).find_ID(self.df['Cell_id'])
+        csv_data = csv_raw_data(file_name, verbose=max(0,verbose-2))
+        self.df = csv_header_body_2_dataframe(csv_data[0], csv_data[1:], verbose=max(0,verbose-2))
+        self.df['Patient_ID'] = Patients_Catalog(verbose=False).find_ID(self.df['Cell_id'], verbose=max(0,verbose-1))
         self.patients_wells_counts = self.df.groupby('Patient_ID').size()
         self.patients_wells_counts.name = 'Num_Wells'
         if verbose:
@@ -20,12 +20,12 @@ class Protein_Data:
 
 
 class Morphology_Data:
-    def __init__(self, file_name, verbose: bool = False) -> None:
+    def __init__(self, file_name, verbose: int = 0) -> None:
         if verbose:
             print(f'Reading Mitochondrial Morphology data: {file_name}')
-        csv_data = csv_raw_data(file_name, verbose=False)
-        self.df = csv_header_body_2_dataframe(csv_data[0], csv_data[1:], verbose=verbose)
-        patients_id = Morphology_Data.group_with_id_2_patient_id(self.df['group_with_id'])
+        csv_data = csv_raw_data(file_name, verbose=max(0,verbose-3))
+        self.df = csv_header_body_2_dataframe(csv_data[0], csv_data[1:], verbose=max(0,verbose-3))
+        patients_id = Morphology_Data.group_with_id_2_patient_id(self.df['group_with_id'], verbose=max(0,verbose-2))
         self.df['Patient_ID'] = patients_id
         self.patients_wells_counts = self.df.groupby('Patient_ID').size()
         self.patients_wells_counts.name = 'Num_Wells'
@@ -33,7 +33,7 @@ class Morphology_Data:
             pd_display(self.patients_wells_counts)
     
     @staticmethod
-    def group_with_id_2_patient_id(column_group_with_id) -> list[str]:
+    def group_with_id_2_patient_id(column_group_with_id, verbose: int = 0) -> list[str]:
         fast_mapping = {}
         possible_groups = ['hc', 'mild', 'severe', 'premanifest']
         patients_id = []
@@ -44,7 +44,7 @@ class Morphology_Data:
                 assert len(new_patients_id) == 1
                 fast_mapping[group_with_id] = new_patients_id[0]
             patients_id.append(fast_mapping[group_with_id])
-        return Patients_Catalog(verbose=False).find_ID(patients_id)
+        return Patients_Catalog(verbose=False).find_ID(patients_id, verbose=verbose)
 
 class All_Mitochondrial_Data:
     def __init__(self, morphology_file_name: str, protain_file_names: list[str], verbose: int = 0) -> None:
